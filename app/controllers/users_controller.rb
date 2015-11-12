2b4b4624864b01a4e6861780a74ecda050da6227
General Assembly | Anthony Ng <generalassemblyanthonyng@Anthonys-MacBook-Air.local>
2015-11-11 19:54:12 -0500

    Update ordering of project posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authorize
-  
+
   def show
     @user = User.find(params[:id])
     @posts = Post.all.where(:user_id == @user.id)
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @project_posts = Post.all.where(category: "project")
+    @project_posts = Post.all.where(category: "project").order(created_at: :desc);
   end
 
 end
