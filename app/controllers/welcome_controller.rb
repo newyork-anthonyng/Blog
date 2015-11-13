bb8e4197f9d9ea3ddb87e0e0dcdc3be48c3df621
General Assembly | Anthony Ng <generalassemblyanthonyng@Anthonys-MacBook-Air.local>
2015-11-13 08:48:12 -0500

    More styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @project_posts = Post.all.where(category: "project").order(created_at: :desc);
+    @project_posts = Post.all.where(category: "project").order(created_at: :desc).limit(5);
   end
 
 end
