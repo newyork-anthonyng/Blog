630d8abe5aad1fc34f7360b07ace84d71f41a28d
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2016-01-07 18:16:24 -0500

    Change controller to only display the 3 most recent projects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @project_posts = Post.all.where(category: "project").order(created_at: :desc).limit(5);
+    @project_posts = Post.all.where(category: "project").order(created_at: :desc).limit(3);
   end
 
   def projects
